ce6ace9a5aa672f114c607b1fdf834f397c42495
victorcv <victorcorreasvillas@gmail.com>
2016-07-22 09:36:31 +0200

    Add project files.

---
+++ Internet Application/Models/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Internet_Application.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Internet_Application.Models
+{
+    public class UserViewModel
+    {
+        [Required,StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(20,MinimumLength = 3),Remote("UsernameAvailable", "User", ErrorMessage = "Nick already exists")]
+        public string Nickname { get; set; }
+
+        [Range(1,3)]
+        public int[] Type { get; set; }
+
+        [Required,EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required, StringLength(20, MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required, StringLength(20, MinimumLength = 5), System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password mismatch")]
+        public string RePassword { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public IEnumerable<UserType> UserTypes { get; set; }
+    }
+}
